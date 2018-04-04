e0d59beefb5817b319285a0d23c9c9baa6cee9b4
lexvenim <lexvenim@gmail.com>
2018-04-04 13:32:20 +0300

    Now in diffirent sizes

--- public/javascripts/Image.js
+++ public/javascripts/Image.js
@@ -4,7 +4,8 @@ var helper = require("./helper")
 class Image {
   constructor(){
     this.color = helper.random(100)
-    this.class = "dog--" + this.color
+    this.size = 25 + helper.random(125)
+    this.classes = ["dog", "color-" + this.color, "size-" + this.size]
     this.dog = new Dog()
   }
 }
